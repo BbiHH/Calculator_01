c3d7ec2a83ad6485d62f86078e8f6fd89d1e9b3d
zhuangbiwang <1347421984@qq.com>
2020-03-19 01:12:45 +0800

    添加测试

---
+++ No_rz43_CalcualtorLib.Test/AddTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using No_rz43_CalcualtorLib.Calculators;
+
+namespace No_rz43_CalcualtorLib.Test
+{
+	[TestClass]
+	public class AddTest
+	{
+		[TestMethod]
+		public void AddTest_TwoAndFour_ReturnSix()
+		{
+			//arrange
+			Add add = new Add();
+			var expected = 6;
+
+			//action
+			var actual = add.Calcualtor(2, 4);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ No_rz43_CalcualtorLib.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("No_rz43_CalcualtorLib.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("No_rz43_CalcualtorLib.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b8e89a8-ed6b-4812-89d9-8b6dbe711273")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
